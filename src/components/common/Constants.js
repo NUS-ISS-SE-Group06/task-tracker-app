1752a30c5a79e56f898158d265921e05f723199b
brittajaison <britta@nus.edu.sg>
2024-10-23 22:07:44 +0800

    changed the urls to api urls prod

--- src/components/common/Constants.js
+++ src/components/common/Constants.js
@@ -1,4 +1,4 @@
 // constants.js
 export const BASE_URL = 'https://w177olau1b.execute-api.us-east-1.amazonaws.com/prd/api'; // Your base URL
-export const HOME_URL = 'https://w177olau1b.execute-api.us-east-1.amazonaws.com/prd/';
+export const HOME_URL = 'http://139.59.218.251:80';
 export const TM_URL = 'https://w177olau1b.execute-api.us-east-1.amazonaws.com/prd/api'; 
