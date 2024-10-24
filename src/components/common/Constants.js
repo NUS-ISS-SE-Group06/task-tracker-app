c6d568417b9d06f37d52d7dca601e29fe7781d11
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-10-24 20:27:13 +0800

    Connecting with nginx ip

--- src/components/common/Constants.js
+++ src/components/common/Constants.js
@@ -1,4 +1,4 @@
 // constants.js
-export const BASE_URL = 'https://w177olau1b.execute-api.us-east-1.amazonaws.com/prd/api'; // Your base URL
+export const BASE_URL = 'http://144.126.240.148'; // Your base URL
 export const HOME_URL = 'http://139.59.218.251:80';
-export const TM_URL = 'https://w177olau1b.execute-api.us-east-1.amazonaws.com/prd/api'; 
+export const TM_URL = 'http://144.126.240.148'; 
